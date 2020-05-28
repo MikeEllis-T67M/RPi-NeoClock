4c15808ad45ed06ee8f478238257a01e5a6c08a1
Mike Ellis <git@mikeellis.org.uk>
2020-05-28 21:12:04 +0100

    Initial commit

---
+++ neoclock32.py
@@ -0,0 +1,64 @@
+import board
+import neopixel
+from astral import LocationInfo
+from astral.sun import sun
+from datetime import datetime as dt
+
+# Set up the main clock face - a ring of 60 LEDs
+clockface            = neopixel.NeoPixel(board.D12, 60)
+clockface.auto_write = False
+
+# Determine where we're using for sunrise/sunset calculations
+city = LocationInfo("London", "England", "Europe/London")
+
+add = lambda x,y: min(x+y,255)
+
+while True:
+    # Turn all the LEDs to dim blue
+    clockface.fill((0, 0, 16))
+
+    # Read the current time and date
+    curtime = dt.now()
+
+    # Calculate the sunrise and sunset times
+    suninfo = sun(city.observer, dt.now())
+
+    sunrise = suninfo["sunrise"]
+    sunset  = suninfo["sunset"]
+
+    # Convert sunrise and sunset to local times
+    sunrise = sunrise + (dt.fromtimestamp(sunrise.timestamp()) - dt.utcfromtimestamp(sunrise.timestamp()))
+    sunset  = sunset  + (dt.fromtimestamp(sunset.timestamp())  - dt.utcfromtimestamp(sunset.timestamp()))
+
+    # Convert sunrise and sunset to LED numbers
+    daystart = int(((sunrise.hour * 60) + sunrise.minute) / 24)
+    dayend   = int(((sunset.hour  * 60) + sunset.minute)  / 24)
+
+    # Set the "day" LEDs to dim yellow
+    for led in range(daystart, dayend):
+        clockface[led] = (4, 4, 0)
+
+    # Get the current time
+    curtime = dt.now()
+
+    # M and S are already in LED number format (0-59), but hours need to be converted
+    s = curtime.second
+    m = curtime.minute
+    h = int((curtime.hour * 60.0 + m) / 12)
+
+    # Make sure the hours are in range, including 1 after 1 one before (=59 after)
+    h1 =  h     % 60
+    h2 = (h+1)  % 60 
+    h3 = (h+59) % 60
+
+    # Add the hands on top of the day/night colours = be careful of byte-overflow!
+    clockface[h1] = tuple(map(add, clockface[h1], (128,0,0)))
+    clockface[h2] = tuple(map(add, clockface[h2], (32,0,0)))
+    clockface[h3] = tuple(map(add, clockface[h3], (32,0,0)))
+
+    clockface[m]  = tuple(map(add, clockface[m], (0,128,0)))
+    clockface[s]  = tuple(map(add, clockface[s], (0,0,255)))
+
+    # Update the display
+    clockface.write()
+    
